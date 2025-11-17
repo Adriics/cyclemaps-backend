ae1b47b20ab873df1808097ad41e064cea9e0235
Adriics <adriancalvachesanchez@gmail.com>
2025-11-17 15:02:32 +0100

    fix: syncronize

--- src/dataSourceConfig.ts
+++ src/dataSourceConfig.ts
@@ -7,7 +7,7 @@ const dataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  synchronize: false,
+  synchronize: true,
   logging: true,
   entities: [__dirname + "/**/*.schema.{ts,js}"],
   migrations: ["src/persistence/**/*.ts"],

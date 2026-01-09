d6d0ce88b848ab7adeff8e143dbc59d4f154eb32
Adriics <adriancalvachesanchez@gmail.com>
2026-01-09 20:40:21 +0100

    feat: update dataSourceConfig

--- src/dataSourceConfig.ts
+++ src/dataSourceConfig.ts
@@ -1,18 +1,16 @@
 import "dotenv/config"
-import { DataSource, DataSourceOptions } from "typeorm"
+import { DataSource } from "typeorm"
 
 const dataSource = new DataSource({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   synchronize: true,
   logging: true,
   entities: [__dirname + "/**/*.schema.{ts,js}"],
   migrations: ["src/persistence/**/*.ts"],
-
-  type: "postgres",
-} as DataSourceOptions)
+})
 
 export default dataSource
